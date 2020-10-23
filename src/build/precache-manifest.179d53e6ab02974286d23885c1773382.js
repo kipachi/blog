17c19832c3bcb20c77d65e6306707260836061df
Aleksandr Latyshevich <a_latyshevich@wargaming.net>
2020-10-23 14:49:25 +0300

    First commit

---
+++ src/build/precache-manifest.179d53e6ab02974286d23885c1773382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa1249b0e1ea47244f1092de42bddff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbbd88ebc15f9467e4c",
+    "url": "/static/css/main.51a720f0.chunk.css"
+  },
+  {
+    "revision": "151828ced28b103b7a2f",
+    "url": "/static/js/2.02064389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02064389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbbd88ebc15f9467e4c",
+    "url": "/static/js/main.6fcdb61d.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
